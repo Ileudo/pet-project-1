b48e8a450327ec54921f55a0218705a8b8affa47
Ilya Udovichenko <rios111@yandex.ru>
2021-10-30 21:35:51 +0300

    feat: start form handling

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -9,10 +9,7 @@ const modals = () => {
 
     triggers.forEach((item) => {
       item.addEventListener('click', (e) => {
-        if (e.target) {
-          e.preventDefault();
-        }
-
+        e.preventDefault();
         btnPressed = true;
 
         if (destroyTrigger) {
@@ -28,13 +25,13 @@ const modals = () => {
         document.body.style.overflow = 'hidden';
         const scrollWidth = calcScroll();
         document.body.style.marginRight = scrollWidth;
-
-        close.addEventListener('click', () => {
-          modal.style.display = 'none';
-          document.body.style.overflow = '';
-          document.body.style.marginRight = '0px';
-        });
       });
+    });
+
+    close.addEventListener('click', () => {
+      modal.style.display = 'none';
+      document.body.style.overflow = '';
+      document.body.style.marginRight = '0px';
     });
 
     modal.addEventListener('click', (e) => {
@@ -42,11 +39,11 @@ const modals = () => {
         windows.forEach((item) => {
           item.style.display = 'none';
         });
-      }
 
-      modal.style.display = 'none';
-      document.body.style.overflow = '';
-      document.body.style.marginRight = '0px';
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+        document.body.style.marginRight = '0px';
+      }
     });
   }
 
